98777d94772ea0a365950d8d82a4490941d7dd57
Sonu Kumar <sv808494@gmail.com>
2025-07-21 22:17:10 +0530

    add some file for deployent

--- myresume/settings.py
+++ myresume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-zko35qyvem806jxoh+96qho_yk-i8$+7(16^%9x0(2i!$!4)x=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -66,7 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-STATIC_URL = '/static/'
 WSGI_APPLICATION = 'myresume.wsgi.application'
 
 
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
